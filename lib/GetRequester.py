fd222662640d43fa897399386fb4e1967bad53a0
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-09-24 07:31:12 +0300

    Completed Assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 class GetRequester:
-
-    def __init__(self, url):
+    def __init__(self, url: str):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self) -> str:
+        response = requests.get(self.url)
+        response.raise_for_status() 
+        return response.text  
 
-    def load_json(self):
-        pass
+    def load_json(self) -> dict:
+        response_body = self.get_response_body()
+        return response.json()  
